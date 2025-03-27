730a92ca5ef5cdecffd17233ef7d405b6fddaecf
pOsdas <ratmir1099@mail.ru>
2025-03-28 01:27:22 +0300

    add some serializers

---
+++ auth_app/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+
+class AuthUserSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    password = serializers.CharField()  # При необходимости преобразовать в Base64
+    refresh_token = serializers.CharField(allow_null=True, required=False)
+    updated_at = serializers.DateTimeField(allow_null=True, required=False)
+
+
+class RegisterUserSerializer(serializers.Serializer):
+    username = serializers.IntegerField()
+    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField()
+
+
+class CombinedUserSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    email = serializers.EmailField()
