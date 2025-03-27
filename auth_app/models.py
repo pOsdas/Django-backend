c393188065cd48dcfd974a66c4d1a7b12d82a5cf
pOsdas <ratmir1099@mail.ru>
2025-03-28 01:27:36 +0300

    add AuthUser model

---
+++ auth_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class AuthUser(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    password = models.BinaryField()
+    refresh_token = models.TextField(null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(id={self.user_id})"
+
+    def __repr__(self) -> str:
+        return str(self)
