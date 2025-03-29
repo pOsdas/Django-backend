c49b6b588ac8936124cf1045c34d4ab30d66c93b
pOsdas <ratmir1099@mail.ru>
2025-03-29 23:05:40 +0300

    add auth_app urls

---
+++ auth_app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .basic_auth import (
+    BasicAuthCredentialsAPIView,
+    RegisterUserAPIView,
+    GetUsersAPIView,
+    BasicAuthUsernameAPIView,
+    CheckTokenAuthAPIView,
+    DeleteAuthUserAPIView,
+)
+
+urlpatterns = [
+    path('basic-auth/', BasicAuthCredentialsAPIView.as_view(), name='basic-auth'),
+    path('register/', RegisterUserAPIView.as_view(), name='register'),
+    path('get_users/', GetUsersAPIView.as_view(), name='get-users'),
+    path('basic-auth-username/', BasicAuthUsernameAPIView.as_view(), name='basic-auth-username'),
+    path('check-token-auth/', CheckTokenAuthAPIView.as_view(), name='check-token-auth'),
+    path('<int:user_id>/', DeleteAuthUserAPIView.as_view(), name='delete-auth-user'),
+]
