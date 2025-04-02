b52398c9242c283183be2f9d367203e9fdb98c8b
pOsdas <ratmir1099@mail.ru>
2025-04-03 00:04:56 +0300

    fix api paths

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from auth_app.config import pydantic_settings
+
+API_PREFIX = pydantic_settings.api.prefix
+API_V1_PREFIX = pydantic_settings.api.v1.prefix
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('auth_app.urls')),
-    path('users/', include('user_app.urls')),
+    path("admin/", admin.site.urls),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/auth/", include("auth_app.api.v1.urls")),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/users/", include("user_app.api.v1.urls")),
 ]
