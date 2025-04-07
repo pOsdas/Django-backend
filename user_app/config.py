0f3274940130de90ae69504a9c19723b5cde1445
pOsdas <ratmir1099@mail.ru>
2025-04-07 23:46:12 +0300

    added config to user_app

---
+++ user_app/config.py
@@ -0,0 +1,57 @@
+from pprint import pprint
+from dotenv import load_dotenv
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# Принудительно загружаем .env и .env-template
+load_dotenv()
+
+
+class RunModel(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 8006
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    users: str = "/users"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_pre_ping: bool = True
+    pool_recycle: int = 600
+    pool_size: int = 50
+
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "pk": "pk_%(table_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env-template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="USER_SERVICE__"
+    )
+    auth_service_url: str
+    run: RunModel = RunModel()
+    api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
+
+
+pydantic_settings = Settings()
+# pprint(pydantic_settings.model_dump())
