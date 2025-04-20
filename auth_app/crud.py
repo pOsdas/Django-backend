38bc29d5957219b6d52567b3d656ebc2afcb2764
pOsdas <ratmir1099@mail.ru>
2025-04-20 18:14:55 +0300

    Some token features have been added and the database of fake users has been removed

--- auth_app/crud.py
+++ auth_app/crud.py
@@ -4,46 +4,20 @@ read
 update
 delete
 """
-from typing import Sequence, Optional
+import httpx
+import redis
+from rest_framework import exceptions
+from typing import Sequence, Optional, Any, Awaitable
 from django.core.exceptions import ObjectDoesNotExist
 
 from auth_app.models import AuthUser
-from auth_app.services.security import hash_password
-
-# ### fake users db
-john = AuthUser(
-    user_id=1,
-    password=hash_password("qwerty"),
-    # updated_at
-    refresh_token="dummy_refresh_token"
-)
-
-sam = AuthUser(
-    user_id=2,
-    password=hash_password("secret"),
-    # updated_at
-    refresh_token="second_dummy_refresh_token"
-)
+from auth_app.config import pydantic_settings as settings
 
 
-test = AuthUser(
-    user_id=3,
-    password=hash_password("test"),
-    # updated_at
-    refresh_token="second_dummy_refresh_token"
-)
-
-users_db: dict[int, AuthUser] = {
-    john.user_id: john,
-    sam.user_id: sam,
-}
-user_id_to_password = {"3": "test"}
-static_auth_token_to_user_id = {
-    "90609ed991fcca984411d4b6e1ba7": john.user_id,
-}
-# ### never do like that
+redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
 
+# ---- users ----
 def get_all_users() -> Sequence[AuthUser]:
     return list(AuthUser.objects.order_by("user_id"))
 
@@ -64,4 +38,41 @@ def delete_auth_user(
     user = get_auth_user(user_id)
     if user:
         user.delete()
+
+
+# ---- tokens ----
+def store_static_token(
+        token: str,
+        user_id: int,
+        ttl: int = 3600
+) -> None:
+    redis_client.set(f"static_auth_token:{token}", user_id, ex=ttl)
+
+
+def get_user_id_by_static_token(token: str) -> int | None:
+    user_id: str = redis_client.get(f"static_auth_token:{token}")
+    return int(user_id) if user_id else None
+
+
+def get_username_by_static_token(request):
+    token = request.headers.get("x-auth-token")
+    if not token:
+        raise exceptions.AuthenticationFailed("Missing token")
+
+    user_id = get_user_id_by_static_token(token)
+    if not user_id:
+        raise exceptions.AuthenticationFailed("Invalid token")
+
+    # Запрос к user_service
+    with httpx.Client(timeout=5.0) as client:
+        response = client.get(
+            f"{settings.user_service_url}/api/v1/users/{user_id}/"
+        )
+
+    if response.status_code != 200:
+        raise exceptions.AuthenticationFailed("User not found")
+
+    user_data = response.json()
+    return user_data["username"]
+
 
