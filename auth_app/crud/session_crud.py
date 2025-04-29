ae478dfc598db2050a71ab455c14296f731ebc53
pOsdas <ratmir1099@mail.ru>
2025-04-29 21:31:13 +0300

    rewritten to use redis (was django_redis)

--- auth_app/crud/session_crud.py
+++ auth_app/crud/session_crud.py
@@ -1,6 +1,15 @@
 import uuid
+import redis
 from django.conf import settings
 from django.core.cache import cache
+
+# Подключение к Redis
+redis_client = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+    decode_responses=settings.REDIS_DECODE_RESPONSES,
+)
 
 SESSION_PREFIX = "session:"
 SESSION_TTL = getattr(settings, "SESSION_TTL", 60 * 60 * 24 * 7)
@@ -8,15 +17,21 @@ SESSION_TTL = getattr(settings, "SESSION_TTL", 60 * 60 * 24 * 7)
 
 def create_session(user_id: int) -> str:
     token = uuid.uuid4().hex
-    cache_key = SESSION_PREFIX + token
-    cache.set(cache_key, {"user_id": user_id}, timeout=SESSION_TTL)
+    key = SESSION_PREFIX + token
+    redis_client.set(key, user_id, ex=SESSION_TTL)
     return token
 
 
-def get_session(token: str) -> dict | None:
-    return cache.get(SESSION_PREFIX + token)
+def get_session(token: str) -> int | None:
+    key = SESSION_PREFIX + token
+    user_id: int = redis_client.get(key)
+    if user_id is not None:
+        # продлеваем жизнь сессии (можете убрать)
+        redis_client.expire(key, SESSION_TTL)
+        return user_id
+    return None
 
 
 def delete_session(token: str) -> None:
-    cache_key = SESSION_PREFIX + token
-    cache.delete(cache_key)
+    key = SESSION_PREFIX + token
+    redis_client.delete(key)
